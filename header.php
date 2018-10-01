780d21e5f15a59a0ce88ca9ae17e7bfa332a321e
muslum <mcoban86@gmail.com>
2018-10-01 16:28:30 +0300

    header seo fixed

--- header.php
+++ header.php
@@ -5,9 +5,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Direction Track</title>
-
+  <title><?php wp_title() ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+
+  <?php wp_head() ?>
 </head>
 
 <body>
