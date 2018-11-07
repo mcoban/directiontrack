6aebb693e5a26894618d6d2b2fcc344c0b432b54
muslum <mcoban86@gmail.com>
2018-11-07 12:35:36 +0300

    ss

--- category.php
+++ category.php
@@ -18,8 +18,6 @@
   <div class="devices">
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut rerum placeat rem cupiditate laborum provident, error similique, aliquid voluptatum autem possimus odio dicta praesentium illo optio nesciunt asperiores vel illum.
   </div>
-
-  
 </div>
 
 <?php get_footer() ?>
