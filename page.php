d2b7e8d99a8499e6c3cc94f57bb519c91e21d8cb
muslum <mcoban86@gmail.com>
2018-11-09 17:59:31 +0300

    css

--- page.php
+++ page.php
@@ -1,12 +1,10 @@
 <?php get_header() ?>
 
 <div id="page">
-    <div class="container large-container">
-      <h1><?php the_title() ?></h1>
-      <div class="content">
-        <?php the_content() ?>
-      </div>
-    </div>
+  <h1><?php the_title() ?></h1>
+  <div class="content">
+    <?php the_content() ?>
+  </div>
 </div>
 
 <?php get_footer() ?>
